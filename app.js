cdf9afa7b9afccf6589dc74442c27496749a62f4
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-24 18:23:03 -0400

    📦 NEW: Set up Sign Up page

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const request = require("request");
-const bodyParser = ("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(express.static("public")); // allows the rendering of "static" pages in public folder
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-  res.send("Server is up and running.")
-})
+  res.sendFile(`${__dirname}/signup.html`);
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  // no log will show unless html form has method & action attr
+  console.log(firstName, lastName, email);
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.")
-})
+});
