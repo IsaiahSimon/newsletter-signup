edab771d5fe234ae9f40c75614c45a2e091d1114
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-25 13:42:55 -0400

    📦 NEW: Update app.js, index.html, style.css
    - JS: Add basic functionality for API post request
    - HTML: add target attr for sign up button
    - CSS: change footer link color

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
+const { response } = require("express");
+
 const app = express();
 
 app.use(express.static("public")); // allows the rendering of "static" pages in public folder
@@ -15,8 +19,46 @@ app.post("/", function (req, res) {
   const lastName = req.body.lName;
   const email = req.body.email;
 
-  // no log will show unless html form has method & action attr
-  console.log(firstName, lastName, email);
+  // Build data
+  const data = {
+    // as per Mailchimp docs Body Parameters
+    members: [                  // must be an array - docs
+      {                         // stores objects - docs
+        email_address: email,   // string (from our form)
+        status: "subscribed",   // string - docs
+        merge_fields: {         // object - docs
+          FNAME: firstName,     // Audience fields and *|MERGE|* tags
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  // Stringify data to be posted to Mailchimp
+  const jsonData = JSON.stringify(data);
+
+  // Variables for https.request
+
+  //const url = "https://<dc>.api.mailchimp.com/3.0/"     // root url for the API, note: must replace <dc> with datacenter in api key
+  const url = `https://${process.env.REGION}.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`     // my lists and datacenter
+
+  const options = {       // https options
+    method: "POST",
+    auth: `isimondev:${process.env.API_KEY}`
+  }
+
+  // https.request(url, options, callback(response from mailchimp))
+  const request = https.request(url, options, function(response) {
+    // check what is returned from mailchimp server
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  });
+
+  // use the request from above
+  request.write(jsonData);
+  request.end();
+
 });
 
 app.listen(3000, function() {
