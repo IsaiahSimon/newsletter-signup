e0ce33f9ef0c0dcb2f2b9fd9bd4379d719da6eba
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-25 15:43:11 -0400

    📦 NEW: Update app.js and *html files
    -JS: Add success and failure routes
    
    -HTML: Create success and failure pages

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.get("/", function(req, res){
   res.sendFile(`${__dirname}/signup.html`);
 });
 
+// Home route...
 app.post("/", function (req, res) {
   const firstName = req.body.fName;
   const lastName = req.body.lName;
@@ -49,6 +50,14 @@ app.post("/", function (req, res) {
 
   // https.request(url, options, callback(response from mailchimp))
   const request = https.request(url, options, function(response) {
+
+    if (response.statusCode == 200) {
+      res.sendFile(`${__dirname}/success.html`);
+    } else {
+      res.sendFile(`${__dirname}/failure.html`);
+    }
+
+
     // check what is returned from mailchimp server
     response.on("data", function(data) {
       console.log(JSON.parse(data));
@@ -56,9 +65,20 @@ app.post("/", function (req, res) {
   });
 
   // use the request from above
-  request.write(jsonData);
+  request.write(jsonData);            // comment out to test failure page
   request.end();
 
+});
+
+
+// Failure route...
+app.post("/failure", function(req, res) {
+  res.redirect("/")
+});
+
+// Success route...
+app.post("/success", function(req, res) {
+  res.redirect("/")
 });
 
 app.listen(3000, function() {
